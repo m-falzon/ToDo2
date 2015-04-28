a148657949273069bb3f71c4c4d086b6c11af600
Matthew Falzon <m.falzon@live.com>
2015-04-28 19:41:25 +1000

    Added Box and Text for login screen

---
+++ ToDo2/LoginViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LoginViewController.swift
+//  ToDo2
+//
+//  Created by Matthew Falzon on 28/04/2015.
+//  Copyright (c) 2015 Matthew Falzon. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    
+}
